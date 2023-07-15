d7548c16c332a5682e0f6e297410b1f996b1070f
Keshav <keshav.kumar@corporateserve.com>
2023-07-15 14:25:18 +0530

    replace text with proper text

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularBasic'`, () => {
+  it(`should have as title 'keshavPortfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularBasic');
+    expect(app.title).toEqual('keshavPortfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularBasic app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('keshavPortfolio app is running!');
   });
 });
