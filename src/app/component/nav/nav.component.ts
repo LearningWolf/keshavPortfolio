ca9c0b3916c8b9dd6c4ae71928f28eaeadf54eb3
Keshav <keshav.kumar@corporateserve.com>
2023-09-24 18:59:43 +0530

    home page completed

--- src/app/component/nav/nav.component.ts
+++ src/app/component/nav/nav.component.ts
@@ -8,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
 
-  navBar = ["home","about me","work Experience","contact"]
-  resume = "My Resume"
-  resumeUrl = "../../../assets/Resume_Keshav.docx"
-  menuBtn = "../../../assets/images/menu-icon.svg"
+  // navBar = ["home","about me","work Experience","contact"];
+  navBar = [
+    {
+      name: "home",
+      link: "#"
+    },
+    {
+      name: "about",
+      link: "#about"
+    },
+    {
+      name: "Experience",
+      link: "#work"
+    },
+    {
+      name: "contact",
+      link: "#contact"
+    },
+    
+  ];
+  // "about me","work Experience","contact"
+  resume = "My Resume";
+  resumeUrl = "../../../assets/Resume_Keshav.docx";
+  menuBtn = "../../../assets/images/menu-icon.svg";
+
   constructor() { }
 
   ngOnInit(): void {
